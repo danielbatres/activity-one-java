85ed757937d55c42aa48af25962091d71996b169
danielbatres <danielbatresuser@gmail.com>
2022-08-09 18:23:25 -0600

    Repo reference

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 import Exercise.Exercise1;
 import Exercise.Exercise2;
 
+/**
+ * Author: Daniel Garcia Batres
+ *
+ * Repository: https://github.com/danielbatres/activity-one-java
+ *
+ * */
+
 public class Main {
     public static void main(String[] args) {
         Exercise1.receiveData();
