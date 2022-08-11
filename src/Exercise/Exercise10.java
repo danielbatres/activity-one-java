b0a5fe3bb987bb8c130a1770fec80999d9c2e0ed
danielbatres <danielbatresuser@gmail.com>
2022-08-11 15:47:22 -0600

    Exercise N°10

---
+++ src/Exercise/Exercise10.java
@@ -0,0 +1,46 @@
+package Exercise;
+
+import Statics.Features;
+import Statics.SC;
+
+public class Exercise10 implements IExercise {
+    private static Client client = new Client();
+    private static float purchaseValue;
+    private static final float DISCOUNT_FIVE = 0.05f;
+    private static final float DISCOUNT_TEN = 0.1f;
+
+    public static void receiveData() {
+        System.out.println("Enter your name");
+        client.setName(SC.sc.nextLine());
+
+        System.out.println("Enter the value of your purchase");
+        purchaseValue = Features.validateDataNumber(purchaseValue);
+
+        purchase();
+    }
+
+    private static float appliedDiscount(float value) {
+        if (value <= 75) return value;
+
+        return (value > 75 && value < 150)
+                ? discount(value, DISCOUNT_FIVE)
+                : discount(value, DISCOUNT_TEN);
+    }
+
+    private static float discount(float value, float discount) {
+        return value - (value * discount);
+    }
+
+    private static void purchase() {
+        float discount = appliedDiscount(purchaseValue);
+
+        printPurchase(client.getName(), purchaseValue, Features.dc.format(discount));
+    }
+
+    private static void printPurchase(String name, float value, String total) {
+        System.out.println("Succesful purchase"
+                + "\nName of the buyer: " + name
+                + "\nInitial value: " + value
+                + "\nTotal value: " + total);
+    }
+}
---
+++ src/Exercise/WorkerClasses.java
@@ -0,0 +1,24 @@
+package Exercise;
+
+public class WorkerClasses {
+    private static final float CLASSA = 7.00f;
+    private static final float CLASSB = 8.50f;
+    private static final float CLASSC = 10.00f;
+    private static final float CLASSD = 12.50f;
+
+    public static float getCLASSA() {
+        return CLASSA;
+    }
+
+    public static float getCLASSB() {
+        return CLASSB;
+    }
+
+    public static float getCLASSC() {
+        return CLASSC;
+    }
+
+    public static float getCLASSD() {
+        return CLASSD;
+    }
+}
