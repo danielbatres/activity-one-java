252a90d58e778e331cd517c1619ff8c2d557cc21
danielbatres <danielbatresuser@gmail.com>
2022-08-11 17:17:04 -0600

    Exercise N°5

--- src/Exercise/Exercise5.java
+++ src/Exercise/Exercise5.java
@@ -1,7 +1,22 @@
 package Exercise;
 
-public class Exercise5 implements IExercise {
-    public static void receiveData() {
+import Statics.Features;
 
+public class Exercise5 implements IExercise {
+    private static int num;
+
+    public static void receiveData() {
+        System.out.println("Enter the integer number");
+        num = Features.validateIntDataNumber(num);
+
+        factorial(num);
+    }
+
+    private static void factorial(int num) {
+        int factorial = 1;
+
+        for (int i = num; i != 0; i--) factorial *= i;
+
+        System.out.println("The factorial is: " + factorial);
     }
 }
