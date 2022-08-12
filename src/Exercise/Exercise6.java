e07deae52130d294c74011b7fa6f9abc6341fadc
danielbatres <danielbatresuser@gmail.com>
2022-08-12 01:17:25 -0600

    Exercise N°6

--- src/Exercise/Exercise6.java
+++ src/Exercise/Exercise6.java
@@ -2,6 +2,23 @@ package Exercise;
 
 public class Exercise6 implements IExercise {
     public static void receiveData() {
+        isPrimeNumber();
+    }
 
+    private static void isPrimeNumber() {
+        boolean prime;
+
+        for (int i = 2; i <= 100; i++) {
+            prime = true;
+            for (int j = i - 1; j > 1; j--) {
+                if (i % j == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+            if (prime) {
+                System.out.println(i);
+            }
+        }
     }
 }
