12aee5a57837a664e0dc4b64e1554a61c4d278fd
danielbatres <danielbatresuser@gmail.com>
2022-08-12 01:40:53 -0600

    Exercise N°4

--- src/Exercise/Exercise4.java
+++ src/Exercise/Exercise4.java
@@ -1,7 +1,27 @@
 package Exercise;
 
-public class Exercise4 implements IExercise {
-    public static void receiveData() {
+import Statics.Features;
 
+public class Exercise4 implements IExercise {
+    private static float salary;
+    private static float rent;
+    private static final float DISCOUNT = 0.1f;
+
+    public static void receiveData() {
+        System.out.println("Enter the rent you pay");
+        rent = Features.validateDataNumber(rent);
+
+        System.out.println("Enter your actual salary");
+        salary = Features.validateDataNumber(salary);
+
+        System.out.println(discount(salary <= 350));
+    }
+
+    private static String discount(boolean salary) {
+        if (!salary) return "Salary doesn't qualify for discount";
+
+        float total = rent - (rent * DISCOUNT);
+
+        return "Total discount is: $" + Features.dc.format(total);
     }
 }
