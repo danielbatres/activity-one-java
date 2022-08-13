ae5ee93783078a79c7c0067dfca93af03ed97f77
danielbatres <danielbatresuser@gmail.com>
2022-08-12 19:18:43 -0600

    Exercise N°8

--- src/Exercise/Exercise8.java
+++ src/Exercise/Exercise8.java
@@ -23,6 +23,8 @@ public class Exercise8 implements IExercise {
             return "The easting coordinate in the first quadrant";
         } else if (fc > 0 && sc > 0) {
             return "The easting coordinate in the second quadrant";
+        } else if (fc == 0 && sc == 0) {
+            return "The coordinate is at the origin";
         }
 
         return "The easting coordinate in the fourth quadrant";
